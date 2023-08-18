ec9e9a05378dca67649d138b17dafa4ad10a57ee
Alsu <alsu.bikkulova@gmail.com>
2023-08-18 13:02:17 +0300

    build.gradle updated

--- src/test/java/ru/netology/card/CardDeliveryTest.java
+++ src/test/java/ru/netology/card/CardDeliveryTest.java
@@ -1,9 +1,13 @@
 package ru.netology.card;
 
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
 import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.Keys;
 
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.visible;
@@ -12,16 +16,23 @@ import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.SetValueOptions.withText;
 
 public class CardDeliveryTest {
+
+    private String generateDate(int addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    }
     @Test
     public void shouldTestDeliveryForm() {
         open("http://localhost:9999");
 
         $("[data-test-id='city'] input").setValue("Казань");
-        $("[data-test-id='date'] input").setValue("19.08.2023");
+        String plannedDate=generateDate(4, "dd.MM.yyyy");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date'] input").setValue(plannedDate);
         $("[data-test-id='name'] input").setValue("Иван Петров");
         $("[data-test-id='phone'] input").setValue("+79000000000");
         $("[data-test-id='agreement']").click();
-        $(".button").click();
-        $("[data-test-id='notification']").shouldBe(visible, Duration.ofSeconds(15));
-    }
+        $("button.button").click();
+        $(".notification__content").shouldBe(visible, Duration.ofSeconds(15));
+        $(".notification__content").shouldHave(Condition.exactText("Встреча успешно забронирована на " + plannedDate));
+        }
 }
